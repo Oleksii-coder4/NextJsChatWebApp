93bdde67633a08afba4c049465f35a9d03d2f527
Oleksii-coder4 <loklom.pp@gmail.com>
2024-07-30 15:34:03 +0300

    Made server, but still with error

---
+++ pages/index.js
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import io from "socket.io-client";
+
+let socket;
+
+const Home = () => {
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    socket = io();
+
+    socket.on("message", (msg) => {
+      setMessages((prevMessages) => [...prevMessages, msg]);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  const sendMessage = () => {
+    socket.emit("message", message);
+    setMessage("");
+  };
+
+  return (
+    <div>
+      <h1>Chat</h1>
+      <input
+        type="text"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      />
+      <button onClick={sendMessage}>Send</button>
+      <ul>
+        {messages.map((msg, index) => (
+          <li key={index}>{msg}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Home;
